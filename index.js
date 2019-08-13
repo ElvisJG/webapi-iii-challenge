34080e704b11f494e71e6cf6efed4bdd3d5b62ca
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-13 19:08:18 -0400

    scaffolding and added logger

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const server = require('./server.js');
+
+port = 4000;
+server.listen(`${port}`, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const express = 'express';
+const express = require('express');
+const postRouter = require('./posts/postRouter');
 
 const server = express();
+const bodyParser = express.json();
+server.use(bodyParser);
+server.use(logger);
+
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+  next();
+}
 
 module.exports = server;
