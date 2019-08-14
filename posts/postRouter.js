b7fdec81b1cdc634620bbe160d5cf7c07919d499
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-13 20:09:43 -0400

    more scaffolding and refactoring to async/await

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -5,22 +5,40 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-  } catch (error) {}
+    const posts = await Posts.find(req.query);
+    res.status(200).json(posts);
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
 });
 
 router.get('/:id', async (req, res) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
 });
 
 router.put('/:id', async (req, res) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
 });
 
 // custom middleware
