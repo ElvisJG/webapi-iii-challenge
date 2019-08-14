545d46d42e4d99f7995f880d41e17041b5b89154
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-13 20:21:42 -0400

    i should look at the file structure before i just go off coding :)

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const Posts = require('./postDb');
+const Users = require('../users/userDb');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const posts = await Posts.find(req.query);
+    const posts = await Posts.get();
     res.status(200).json(posts);
   } catch (error) {
     // log error
@@ -43,29 +44,6 @@ router.put('/:id', async (req, res) => {
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  const { uid } = req.user;
-}
-
-function validateUserId(req, res, next) {
-  const { id } = req.params;
-
-  Posts.getById(id)
-    .then(post => {
-      post ? next() : res.status(404).json({ message: 'invalid user ID' });
-    })
-    .catch(error => {
-      // log error
-      console.log(error);
-      res.status(500).json({ message: 'Error processing request' });
-    });
-}
-
-function validateUser(req, res, next) {
-  req.body ? next() : res.status(400).json({ message: 'missing post data' });
-  !req.body.name
-    ? res.status(400).json({ message: ' missing required text field' })
-    : next();
-}
+function validatePostId(req, res, next) {}
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,46 +2,59 @@ const express = 'express';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
-
-function validateUser(req, res, next) {
-
-};
+function validateUser(req, res, next) {}
 
 function validatePost(req, res, next) {
-
-};
+  if (!req.body) {
+    res.status(400).json({ message: 'missing post data' });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: 'missing required text field' });
+  }
+  next();
+  //  ⬇️ This won't work RIP ⬇️
+  //   req.body ? next() : res.status(400).json({ message: 'missing post data' });
+  //   !req.body.name
+  //     ? res.status(400).json({ message: ' missing required text field' })
+  //     : next();
+}
 
 module.exports = router;
